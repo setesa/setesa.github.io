0df39fd34caf2841ba9c371c29c086c44c9d4d05
setesa <akunclasstjkt1@gmail.com>
2024-10-19 20:52:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function searchName() {
   
     // Data dummy disimpan dalam array of objects
     const dataDummy = [
-      { name: "fachri", description: "Fachri adalah ketua kelas kita semasa SMK, dia orangnya banyak omong, ngatur, tapi tidak galak, kadang suka membantu temannya." },
-      { name: "ahmad", description: "Ahmad adalah teman baik kita yang selalu rajin dan pandai, dia sering membantu teman lain dalam pelajaran." },
-      { name: "siti", description: "Siti adalah teman sekelas yang sangat tenang dan sabar, selalu membantu jika ada yang butuh pertolongan." },
+      { name: "fachri","fachri ahsani", description: "Fachri Ahsani adalah ketua kelas kita semasa SMK, dia orangnya banyak omong, ngatur, tapi tidak galak, kadang suka membantu temannya." },
+      { name: "saniya","saniya wardah musbita", description: "Saniya Wardah Musbita adalah teman baik kita yang selalu rajin dan pandai, dia sering membantu teman lain dalam pelajaran." },
+      { name: "hafis nugroho", description: "Hafis nugroho adalah teman sekelas yang sangat tenang dan sabar, selalu membantu jika ada yang butuh pertolongan." },
       { name: "nurul", description: "Nurul adalah seorang yang ceria dan selalu membawa energi positif ke dalam kelas." },
       { name: "yusuf", description: "Yusuf dikenal sebagai orang yang sangat pendiam, tapi pandai dan berbakat dalam matematika." },
       { name: "budi", description: "Budi adalah teman yang selalu humoris dan suka membuat suasana menjadi menyenangkan di kelas." },
